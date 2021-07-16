0bd44757225149a06825bf8c3d4ac5a745719764
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-16 22:25:54 +0530

    [Harshitha] Reflection using Default Constructor

--- MoodAnalyzerProblem/CustomMoodAnalyser.cs
+++ MoodAnalyzerProblem/CustomMoodAnalyser.cs
@@ -12,7 +12,6 @@ namespace MoodAnalyzerProblem
         public enum ExceptionType
         {
             NULL_EXCEPTION, EMPTY_EXCEPTION, 
-            CONSTRUCTOR_NOT_FOUND,CLASS_NOT_FOUND,
             NO_SUCH_CLASS,NO_SUCH_METHOD
         }
 
